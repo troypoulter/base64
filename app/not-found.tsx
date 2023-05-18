9d3892e651b3da0f13c34a562b504df19a5f7877
Troy Poulter <troypoulter@gmail.com>
2023-05-18 11:33:05 +0000

    add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link"
+
+export default function NotFound() {
+  return (
+    <section className="container grid items-center gap-6 py-6 md:pt-10">
+      <div className="flex flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+          404 not found.
+        </h1>
+        <p className="text-lg text-muted-foreground sm:text-xl">
+          Head back{" "}
+          <Link className="underline underline-offset-4" href="/" passHref>
+            Home
+          </Link>{" "}
+          and try again.
+        </p>
+      </div>
+    </section>
+  )
+}
