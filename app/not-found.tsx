bb01186105c8f9fc5b09807ae31b8dbc996838de
Troy Poulter <troypoulter@gmail.com>
2023-05-18 22:44:28 +1000

    update 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,4 @@
-import { Metadata } from "next"
 import Link from "next/link"
-
-export const metadata: Metadata = {
-  title: "404 Not Found",
-}
 
 export default function NotFound() {
   return (
@@ -13,13 +8,6 @@ export default function NotFound() {
           <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
             404 not found.
           </h1>
-          <p className="text-lg text-muted-foreground sm:text-xl">
-            Head back{" "}
-            <Link className="underline underline-offset-4" href="/" passHref>
-              Home
-            </Link>{" "}
-            and try again.
-          </p>
         </div>
       </section>
     </div>
