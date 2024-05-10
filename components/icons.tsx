d76b6aa6d122ed40290f2ccbe7174e41c2b95fd7
troypoulter <troypoulter@gmail.com>
2024-05-10 17:08:13 +1000

    fix icon type

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,22 +1,27 @@
-import {
-  LucideProps,
-  Moon,
-  SunMedium,
-  Laptop2,
-  type Icon as LucideIcon,
-  Code,
-} from "lucide-react"
+import { Code, Moon, SunMedium } from "lucide-react"
 
-export type Icon = LucideIcon
+type IconProps = React.HTMLAttributes<SVGElement>
 
 export const Icons = {
   sun: SunMedium,
   moon: Moon,
-  website: (props: LucideProps) => (
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M20 16V7a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v9m16 0H4m16 0 1.28 2.55a1 1 0 0 1-.9 1.45H3.62a1 1 0 0 1-.9-1.45L4 16"></path></svg>
+  website: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M20 16V7a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v9m16 0H4m16 0 1.28 2.55a1 1 0 0 1-.9 1.45H3.62a1 1 0 0 1-.9-1.45L4 16"></path>
+    </svg>
   ),
   logo: Code,
-  gitHub: (props: LucideProps) => (
+  gitHub: (props: IconProps) => (
     <svg viewBox="0 0 438.549 438.549" {...props}>
       <path
         fill="currentColor"
@@ -25,4 +30,3 @@ export const Icons = {
     </svg>
   ),
 }
-
